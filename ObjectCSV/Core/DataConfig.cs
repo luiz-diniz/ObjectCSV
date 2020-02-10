6ef549c09557275cad5ee075df216c977402cca1
luiz-diniz <39447716+luiz-diniz@users.noreply.github.com>
2020-02-09 23:14:14 -0300

    Add files via upload
    ObjectCSV V1
    
    Using a simple console application.

---
+++ ObjectCSV/Core/DataConfig.cs
@@ -0,0 +1,38 @@
+﻿//using System;
+//using System.Collections.Generic;
+
+//namespace ObjectCSV
+//{
+//    public class DataConfig
+//    {
+//        public List<string> GetModel<T>(T type)
+//        {
+//            Console.WriteLine("Creating Data Model.");
+
+//            List<string> modelList = new List<string>();
+
+//            foreach (var prop in type.GetType().GetProperties())
+//            {
+//                modelList.Add(prop.ToString());
+//            }
+
+//            var formatedList = FormatModel(modelList);
+
+//            return formatedList;
+//        }
+
+//        public List<string> FormatModel(List<string> modelData)
+//        {
+//            Console.WriteLine("Formating Data Model.");
+
+//            for (int i = 0; i < modelData.Count; i++)
+//            {
+//                string actualValue = modelData[i];
+//                string finalChar = actualValue.Substring(actualValue.IndexOf(" ") + 1);
+//                modelData[modelData.FindIndex(x => x.Equals(actualValue))] = finalChar;            
+//            }
+
+//            return modelData;
+//        }
+//    }
+//}
---
+++ ObjectCSV/Core/Program.cs
@@ -0,0 +1,29 @@
+﻿using ObjectCSV.Repository;
+using System;
+using System.Collections.Generic;
+
+namespace ObjectCSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var csvConfig = new CSVConfig();
+
+            var person = new List<Person>
+            {
+                new Person { Id = 1, Name = "Joaquim", BirthDate = DateTime.Parse("05/10/1984 12:00:30 PM"), Size = 1.50 },
+                new Person { Id = 2, Name = "Joker", BirthDate = DateTime.Parse("6/11/1964 2:20:34 AM"), Size = 1.50 },
+                new Person { Id = 3, Name = "Phoenix", BirthDate = DateTime.Parse("9/2/1999 10:54:45 AM"), Size = 1.50 }
+            };
+
+            Console.WriteLine("Path with file name: ");
+            string pathPerson = Console.ReadLine();
+
+            csvConfig.WriteOnCSV(person, pathPerson);
+
+            Console.WriteLine("\n\nData exported sucessfuly.");
+            Console.ReadKey();
+        }
+    }
+}
